9c390754330ccba254253cf44b0b33e76e47f315
MustafaDAG33 <mustafadaggg44@gmail.com>
2022-12-27 19:57:33 +0300

    27-12-2022

---
+++ src/test/java/automation_exercise/E06.java
@@ -0,0 +1,71 @@
+package automation_exercise;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class E06 {
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+        //1. Launch browser
+        //2. Navigate to url 'http://automationexercise.com'
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("http://automationexercise.com");
+    }
+
+    @Test
+    public void ContactUsForm(){
+
+        //3. Verify that home page is visible successfully
+        Assert.assertTrue(driver.findElement(By.xpath("//html[@lang='en']")).isDisplayed());
+
+        //4. Click on 'Contact Us' button
+        driver.findElement(By.xpath(" //*[text()=' Contact us']")).click();
+
+        //5. Verify 'GET IN TOUCH' is visible
+        WebElement gt = driver.findElement(By.xpath("(//h2[@class='title text-center'])[2]"));
+        System.out.println(gt.getText());
+        Assert.assertTrue(gt.isDisplayed());
+
+        //6. Enter name, email, subject and message
+        driver.findElement(By.xpath("//input[@data-qa='name']")).sendKeys("Messi", Keys.TAB,"messi@gmail.com",
+                Keys.TAB, "Tebrik",Keys.TAB, "Hello Messi");
+
+        //7. Upload file
+        //8. Click 'Submit' button
+        driver.findElement(By.xpath("//input[@data-qa='submit-button']")).click();
+
+        //9. Click OK button
+        driver.switchTo().alert().accept();
+
+        //10. Verify success message 'Success! Your details have been submitted successfully.' is visible
+        WebElement messageelement =   driver.findElement(By.xpath("//div[@class='status alert alert-success']"));
+        Assert.assertTrue(messageelement.isDisplayed());
+
+
+        //11. Click 'Home' button and verify that landed to home page successfully
+        driver.findElement(By.xpath("(//*[text()=' Home'])[2]")).click();
+        driver.navigate().back();
+        driver.navigate().forward();
+        Assert.assertTrue(driver.findElement(By.xpath("//html[@lang='en']")).isDisplayed());
+        driver.close();
+
+    }
+
+
+
+
+}
---
+++ src/test/java/automation_exercise/E09.java
@@ -0,0 +1,74 @@
+package automation_exercise;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class E09 {
+
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("http://automationexercise.com");
+
+    }
+
+    @Test
+    public void searchProductTest(){
+
+        //1. Launch browser
+        //2. Navigate to url 'http://automationexercise.com'
+        //3. Verify that home page is visible successfully
+        WebElement homePage = driver.findElement(By.xpath("//html[@lang='en']"));
+        Assert.assertTrue(homePage.isDisplayed());
+
+
+        //4. Click on 'Products' button
+        driver.findElement(By.xpath("//a[@href='/products']")).click();
+        driver.navigate().back();
+        driver.navigate().forward();
+
+        //5. Verify user is navigated to ALL PRODUCTS page successfully
+        String expectedTitle = "ALL PRODUCTS";
+        String actualTitle  = driver.getTitle().toUpperCase();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+        //6. Enter product name in search input and click search button
+        WebElement webElement =   driver.findElement(By.xpath("//input[@id='search_product']"));
+        webElement.sendKeys("Tshirt");
+
+        driver.findElement(By.xpath("//*[@class='fa fa-search']")).click();
+
+        //7. Verify 'SEARCHED PRODUCTS' is visible
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@class='title text-center']")).isDisplayed());
+
+        //8. Verify all the products related to search are visible
+        List<WebElement> productList = driver.findElements(By.xpath("//*[text()='View Product']"));
+
+        for(WebElement w: productList){
+            Assert.assertTrue(w.isDisplayed());
+
+        }
+
+        driver.close();
+
+
+
+    }
+}
